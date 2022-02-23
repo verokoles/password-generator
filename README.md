2057891cd11eb15eccf5515581d149cded7b3ada
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-02-22 19:20:52 -0600

    password attempt

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Password Generator Starter Code
+# Password Generator
+## Purpose:
+ * To create an application with JavaScript that contains a password generator. 
+ * Any user can utilize to create their own unnique, random passcode that meets certain criteria.
+ * It will run in the browser.
+
+## Usage
+ * CSS
+ * HTML
+ * JavsScript
+
+ ## LINKS
+  *MDN Web Docs on JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+  *
+  *
+  *
+  *
+ 
+
--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// Assignment code here
-
-
+// create function generatePassword
+function generatePassword() {
+  var str = ""
+  var pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz1234567890' + '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+  for (i = 0; i == 8; i++) {
+    var randomstr = Math.ceil(Math.random() * pass.length + 1);
+    str += pass.charAt(randomstr);
+    console.log(str)
+    return str
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +23,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+

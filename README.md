823c75983e4ada63a2415bdbed5594acc48561b2
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-02-24 22:27:27 -0600

    password gen update

--- README.md
+++ README.md
@@ -9,6 +9,10 @@
  * HTML
  * JavsScript
 
+ ## Steps
+ An issue that would/t allow the code to run was the incorrect placement of the _____ outside of the 'for' loop. Variables created within the curly braces of a function only exist within that function's braces. Any reference to it outside of the function will cause the program to break because it can't find a variable with that name. being outside of the 'for' loop kept only drawing random groups of two, one, and zero characters. Once I placed them inside of the 'for' loop
+
+
  ## LINKS
   *MDN Web Docs on JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript
   *Help on geeksforgeeks https://www.geeksforgeeks.org/how-to-generate-a-random-password-using-javascript/
--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-// create function generatePassword
+
+//  create function generatePassword
 function generatePassword() {
-  var str = ""
-  var pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz1234567890' + '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
-  for (i = 0; i = 8; i++) {
-    var randomstr = Math.ceil(Math.random() * pass.length + 1);
-    str += pass.charAt(randomstr);
-    console.log(str)
-    return str
+  console.log("hello! You have clicked the button!");
+  
+  // the user is prompted to type password according to criteria
+  //    -pass length is between 8 and 128
+  //    -numeric, uppercase, lowercase, special characters
+validateNumber();
+return str;
+
+}
+
+function validateNumber() {
+   var result = window.prompt("How long do you want your password to be? Note: It must be between 8 and 128 characters!");
+   var str = '';
+   if (result < 8) {
+    window.alert("Password should be greater than 8 characters and no more than 128!");  
   }
+ else if (result > 128) {
+   window.alert("Password must be no more than 128 characters!");
+ }
+ else {
+  var pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz1234567890' + '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+  for (i = 1; i <= result; i++) {
+    var randomstr = Math.ceil((Math.random() * pass.length) + 1);
+    str += pass.charAt(randomstr);
+   }
+}
+  // generate password based on the criteria
+console.log(str);
+  // validate what the user inserts
+  // display the unique password on onto page
+
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -18,7 +43,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+console.log(action);
 }
 
 // Add event listener to generate button

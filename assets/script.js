1fbb682aea04252ca78a2a29efb44816c7360d82
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-02-26 11:22:20 -0600

    arrays and prompts created

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,46 @@
+var upperCase = "";
+var lowerCase = "";
+var numeric = "";
+var specialChar = "";
 
-//  create function generatePassword
-
+//  create function to validate Password
 function validatePassword() {
   var result = window.prompt("How long do you want your password to be? Note: It must be between 8 and 128 characters!");
   var str = '';
+  // if user types in any number less than 8 they get an alert to choose between 8 and 128
   if (result < 8) {
     str = "wrong selection";
     window.alert("Password should be greater than 8 characters and no more than 128!");
   }
+  // if user types in any number more than 128 they get an alert to type a number
+  // less no greater than 128
   else if (result > 128) {
     str = "wrong selection";
     window.alert("Password must be no more than 128 characters!");
-  }
-  else {
-    var pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz1234567890' + '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
-    for (i = 1; i <= result; i++) {
-      var randomstr = Math.ceil((Math.random() * pass.length) + 1);
-      str += pass.charAt(randomstr);
-    }
-    return str;
+    
+    //next few pop-ups prompt user to choose whether they want or don't want
+    // uppercase, lowercase, numbers, and/or special characters
+  } else {
+   askUpperCase = confirm("Do you want to use uppercase letters?");
+   askLowerCase = confirm("Do you want to use lowercase letters?");
+   askNumeric = confirm("Do you want to use numbers?");
+   askSpecialChar = confirm("Do you want to use special characters?");
+  };
+
+  // else {
+  //   var pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz1234567890' + '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+  //   for (i = 1; i <= result; i++) {
+  //     var randomstr = Math.ceil((Math.random() * pass.length) + 1);
+  //     str += pass.charAt(randomstr);
+  //   }
+  //   return str;
   }
   // generate password based on the criteria
-  console.log(str);
-  // validate what the user inserts
+  // console.log(str);
+
+
+// validate what the user inserts
   // display the unique password on onto page
-}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
